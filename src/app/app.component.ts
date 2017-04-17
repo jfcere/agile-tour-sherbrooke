9000c81bc674f6972ce322ba340b87420021e37d
jfcere <jfcere@sherweb.com>
2017-04-17 16:13:49 -0400

    Fix responsive schedule table loading on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements AfterViewInit {
+  @ViewChild('tableContainer') tableContainer: ElementRef
+
+  constructor(private renderer: Renderer) { }
+
+  ngAfterViewInit() {
+    // ugly fix for mobile as fixed column in table was not correctly positioned
+    // due to loading time of other element such as fonts and images
+    setTimeout(() => {
+      this.renderer.setElementClass(this.tableContainer.nativeElement, 'table-container', true);
+    }, 1000);
+  }
+}
