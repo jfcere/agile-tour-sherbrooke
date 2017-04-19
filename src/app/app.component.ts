b3fc4131714e3b74e1297742ebec2212d0647bd8
jfcere <jfcere@sherweb.com>
2017-04-19 00:43:45 -0400

    Added schedule table details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { AfterViewInit, Component, ElementRef, Renderer, ViewChild } from '@angular/core';
 
+interface IConference {
+  title: string,
+  presenter: string,
+  company: string,
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +13,75 @@ import { AfterViewInit, Component, ElementRef, Renderer, ViewChild } from '@angu
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('tableContainer') tableContainer: ElementRef
+
+  conferenceAm1: IConference[] = [
+    {
+      title: 'Faire Agile vs être Agile',
+      presenter: 'Olivier Fortier',
+      company: 'FX Innovation',
+    },
+    {
+      title: '"Scaling Agile" dans un contexte manufacurier',
+      presenter: 'Benoit Lapointe',
+      company: 'IBM',
+    },
+    {
+      title: 'Top 5 des meilleures façons d\'améliorer votre code',
+      presenter: 'Éric De Carufel',
+      company: 'Pyxis',
+    },
+    {
+      title: 'Le Management 3.0: Des processus qui marche',
+      presenter: 'Nathalie Ferron et Magali Doucet',
+      company: 'Levio',
+    },
+  ];
+
+  conferencePm1: IConference[] = [
+    {
+      title: 'Gérer l\'inconnu avec peu de moyens par le développement itératif - L\'Agilité chez PMCtire.com',
+      presenter: 'Benoit St-André & Frédéric Gauthier-Boutin',
+      company: 'PMCtire',
+    },
+    {
+      title: 'CGI Sherbrooke: transformation agile en cours',
+      presenter: 'Sedera Randria',
+      company: 'CGI',
+    },
+    {
+      title: 'La valeur: édition du développeur',
+      presenter: 'Luc St-Laurent',
+      company: 'Pyxis',
+    },
+    {
+      title: 'En route vers l\'optimisation! Votre succès avec Kanban étape par étape',
+      presenter: 'Valéry Germain & Nicolas Mercier',
+      company: 'Facilité',
+    },
+  ];
+
+  conferencePm2: IConference[] = [
+    {
+      title: 'CI: Intégration continue, de la théorie à la pratique',
+      presenter: 'Gabriel Blais-Bourget',
+      company: 'Sherweb',
+    },
+    {
+      title: 'Revoir le travail du gestionnaire du passé au présent',
+      presenter: 'Daniel Prince',
+      company: 'Revenu Québec',
+    },
+    {
+      title: 'Architecture express pour petits projets',
+      presenter: 'Frédéric Paquet & Eric Lessard',
+      company: 'Facilité',
+    },
+    {
+      title: 'A déterminer',
+      presenter: '',
+      company: '',
+    },
+  ];
 
   constructor(private renderer: Renderer) { }
 
